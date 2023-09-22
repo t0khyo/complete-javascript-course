e03e42e6b6c7aab04008ced7348f586067b0f07a
t0khyo <abdullrhman.eltoukhy@gmail.com>
2023-09-22 03:25:01 +0300

    First commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,89 @@
+/*
+let js = 'amazing';
+
+40+20-10+23;
+console.log(40 + 20 - 10 + 23);
+
+console.log('T0khyo');
+console.log(12);
+
+let firsName = 'Abdelrahman';
+console.log(firsName);
+
+let jsIsFun = true;
+console.log(jsIsFun);
+
+console.log(typeof jsIsFun);
+// console.log(typeof firsName);
+
+jsIsFun = 'YES!';
+console.log(jsIsFun);
+console.log(typeof jsIsFun);
+
+let year;
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+
+console.log(typeof null);
+*/
+
+/*
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 2000;
+// const job;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Eltouky';
+console.log(lastName);
+*/
+
+/*
+// Math operators
+const now = 2023;
+const ageAbdelrahman = now - 2004;
+const ageMohamed = now - 1969;
+console.log(ageAbdelrahman, ageMohamed);
+
+console.log(ageAbdelrahman * 2, ageAbdelrahman/10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3
+
+const firsName = 'Abdelrahman';
+const lastName = 'Eltouky';
+console.log(firsName + ' ' + lastName);
+
+// Assignment operators
+let x = 10 + 5;
+console.log(x);
+x += 10;
+x *= 4;
+x++;
+x--;
+console.log(x);
+
+// comparison operators
+console.log(ageAbdelrahman > ageMohamed);
+console.log(ageAbdelrahman >= 18);
+*/
+
+const firsName = 'Abdelrahman';
+const job = 'developer';
+const birthYear = 2004;
+const year = 2029;
+
+const Abdelrahman = "I'm " + firsName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
+console.log(Abdelrahman);
+
+const newAbdelrahman = `I'm ${firsName}, a ${year - birthYear} years old ${job}!`;
+
+console.log('string\nwith\nmultiple\nlines');
+
+console.log(`String
+multiple
+lines`);
