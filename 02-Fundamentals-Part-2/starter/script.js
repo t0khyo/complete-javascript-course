38081c928e2d18e563f65f28bb0ccd67673b6083
t0khyo <abdullrhman.eltoukhy@gmail.com>
2023-09-28 18:41:59 +0300

    html&css crash course

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 /*
 let hasDriversLicense = false;
 let passTest = true;
@@ -159,8 +159,9 @@ console.log(t0khyo.calcAge());
 
 let dice = Math.trunc(Math.random() * 6) + 1;
 
-while(dice !== 6){
-    console.log(`you rolled a ${dice}`)
-    dice = Math.trunc(Math.random() * 6) + 1;
-    if(dice === 6) console.log('loop is about to end...');
+while (dice !== 6) {
+  console.log(`you rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+
+  if (dice === 6) console.log("loop is about to end...");
 }
